fb76cce2000e513ad49e637fe84a0e35873be0b2
alekzonder <alekzonder@gmail.com>
2022-07-22 14:14:47 +0700

    chg: migrate Markdown Api class to ts

--- src/api/Markdown.ts
+++ src/api/Markdown.ts
@@ -1,271 +1,193 @@
-'use strict';
+import fs from "fs-extra";
+import MarkdownIt from "markdown-it";
+import highlightJs from "highlight.js";
 
-var path = require('path');
-var fs = require('fs-extra');
+import { Logger, ConfigType } from "@/util";
+import Fs from "./Fs";
 
-var MarkdownIt = require('markdown-it');
+// var plugins = {
+//     taskLists: require('markdown-it-task-lists'),
+//     emoji: require('markdown-it-emoji'),
+//     footnote: require('markdown-it-footnote'),
+//     ins: require('markdown-it-ins'),
+//     sup: require('markdown-it-sup'),
+//     sub: require('markdown-it-sub'),
+//     anchor: require('markdown-it-anchor'),
+//     mark: require('markdown-it-mark'),
+//     // abbr: require('markdown-it-abbr')
+// };
+// var toc = require('markdown-toc');
 
-var plugins = {
-    taskLists: require('markdown-it-task-lists'),
-    emoji: require('markdown-it-emoji'),
-    footnote: require('markdown-it-footnote'),
-    ins: require('markdown-it-ins'),
-    sup: require('markdown-it-sup'),
-    sub: require('markdown-it-sub'),
-    anchor: require('markdown-it-anchor'),
-    mark: require('markdown-it-mark'),
-    // abbr: require('markdown-it-abbr')
-};
-
-var highlightJs = require('highlight.js');
-var toc = require('markdown-toc');
+const plugins = {};
 
 /**
  * Markdown render class
  */
 export default class Markdown {
+  protected _logger: Logger;
 
-    /**
-     * @param {logger} logger
-     * @param {config} config
-     * @param {Object} api
-     */
-    constructor (logger, config, api) {
-        this._logger = logger;
-        this._config = config;
-        this._api = api;
+  protected _config: ConfigType;
 
-        this._md = null;
+  protected _fsApi: Fs;
 
-        this._init();
-    }
+  protected _md: MarkdownIt;
 
-    /**
-     * render markdown from text string
-     *
-     * @param {String} text
-     * @return {Promise}
-     */
-    renderText (text) {
+  constructor(logger: Logger, config: ConfigType, fsApi: Fs) {
+    this._logger = logger;
+    this._config = config;
+    this._fsApi = fsApi;
 
-        return new Promise((resolve, reject) => {
+    this._md = this._initMarkdownIt();
+  }
 
-            const tocOptions = {
-                bullets: '-',
-                slugify: this._slugify
-            };
+  async renderText(text: string) {
+    //   const tocOptions = {
+    //     bullets: "-",
+    //     slugify: this._slugify,
+    //   };
 
-            text = toc.insert(text, tocOptions);
+    //   text = toc.insert(text, tocOptions);
 
-            var rendered = this._md.render(text);
+    return this._md.render(text);
+  }
 
-            resolve(rendered);
+  renderFile(
+    filepathFrom: string,
+    filepathTo: string,
+    options: any = {}
+  ): Promise<{ from: string; to: string }> {
+    return this._read(filepathFrom)
+      .then((data) => this.renderText(data))
+      .then((_text) => {
+        let text = _text;
 
-        });
-
-    }
-
-    /**
-     * render markdown file
-     *
-     * @param {String} filepathFrom
-     * @param {String} filepathTo
-     * @param {Object} options
-     * @return {Promise}
-     */
-    renderFile (filepathFrom, filepathTo, options) {
-
-        return new Promise((resolve, reject) => {
-
-            if (!options) {
-                options = {};
-            }
-
-            this._read(filepathFrom)
-                .then((data) => {
-                    return this.renderText(data);
-                })
-                .then((text) => {
-
-
-                    if (typeof options.header === 'string') {
-                        text = options.header + text;
-                    }
-
-                    if (typeof options.prefooter === 'string') {
-                        text = text + options.prefooter;
-                    }
-
-                    if (typeof options.footer === 'string') {
-                        text = text + options.footer;
-                    }
-
-                    text = text + '</body></html>';
-
-                    return this._save(filepathTo, text);
-                })
-                .then(() => {
-                    resolve({
-                        from: filepathFrom,
-                        to: filepathTo
-                    });
-                })
-                .catch((error) => {
-                    reject(error);
-                });
-        });
-
-    }
-
-    watch (dir, options) {
-
-        if (!options) {
-            options = {};
+        if (typeof options.header === "string") {
+          text = options.header + text;
         }
 
-        var watcher = this._api.fs.watchdir(dir);
+        if (typeof options.prefooter === "string") {
+          text += options.prefooter;
+        }
 
-        watcher.on('add', (filepath) => {
-            var to = this._api.fs.getRenderPath(filepath);
+        if (typeof options.footer === "string") {
+          text += options.footer;
+        }
 
-            if (!this._api.fs.check(filepath)) {
-                return;
-            }
+        text += "</body></html>";
 
-            this.renderFile(filepath, to, options)
-                .then(() => {
-                    this._logger.info(`render file: ${filepath} => ${to}`);
-                }).catch((error) => {
-                    this._logger.error(error);
-                });
+        return this._save(filepathTo, text);
+      })
+      .then(() => ({
+        from: filepathFrom,
+        to: filepathTo,
+      }));
+  }
+
+  watch(dir: string, options: any = {}) {
+    const watcher = this._fsApi.watchdir(dir);
+
+    const render = (filepath: string) => {
+      const to = this._fsApi.getRenderPath(filepath);
+
+      if (!this._fsApi.check(filepath)) {
+        return;
+      }
+
+      this.renderFile(filepath, to, options)
+        .then(() => {
+          this._logger.info(`render file: ${filepath} => ${to}`);
+        })
+        .catch((error) => {
+          this._logger.error(error);
         });
+    };
 
-        watcher.on('change', (filepath) => {
-            var to = this._api.fs.getRenderPath(filepath);
+    watcher.on("add", render);
+    watcher.on("change", render);
 
-            if (!this._api.fs.check(filepath)) {
-                return;
-            }
+    return watcher;
+  }
 
-            this.renderFile(filepath, to, options)
-                .then(() => {
-                    this._logger.info(`render file: ${filepath} => ${to}`);
-                }).catch((error) => {
-                    this._logger.error(error);
-                });
-        });
+  _read(filepath: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      fs.readFile(filepath, (error, data) => {
+        if (error) {
+          return reject(error);
+        }
 
-        return watcher;
-    }
+        return resolve(data.toString());
+      });
+    });
+  }
 
-    /**
-     * @private
-     * @param {String} filepath
-     * @return {Promise}
-     */
-    _read (filepath) {
+  _save(filepath: string, data: any) {
+    return new Promise((resolve, reject) => {
+      fs.outputFile(filepath, data, (error) => {
+        if (error) {
+          return reject(error);
+        }
 
-        return new Promise((resolve, reject) => {
+        return resolve(filepath);
+      });
+    });
+  }
 
-            fs.readFile(filepath, (error, data) => {
-                if (error) {
-                    return reject(error);
-                }
+  _initMarkdownIt(): MarkdownIt {
+    return new MarkdownIt({
+      highlight: (code, type) => {
+        let hl = code;
 
-                resolve(data.toString());
-            });
+        try {
+          if (type) {
+            hl = highlightJs.highlight(type, code).value;
+          } else {
+            hl = highlightJs.highlightAuto(code).value;
+          }
+        } catch (error) {
+          this._logger.error(error);
+        }
 
-        });
+        return hl;
+      },
 
-    }
+      typographer: true,
+      linkify: true,
+      html: true,
+    });
 
-    /**
-     * @private
-     * @param {String} filepath
-     * @param {String} data
-     * @return {Promise}
-     */
-    _save (filepath, data) {
+    // this._md.use(plugins.taskLists, { label: true });
+    // this._md.use(plugins.emoji);
+    // this._md.use(plugins.footnote);
+    // this._md.use(plugins.sub);
+    // this._md.use(plugins.sup);
+    // this._md.use(plugins.ins);
+    // this._md.use(plugins.mark);
+    // this._md.use(plugins.abbr);
+    // this._md.use(mdContainer);
 
-        return new Promise((resolve, reject) => {
+    // this._md.use(plugins.anchor, {
+    //   level: 1,
+    //   slugify: this._slugify,
+    //   permalink: true,
+    //   permalinkClass: "header-anchor",
+    //   permalinkSymbol: "#",
+    //   permalinkBefore: false,
+    // });
 
-            fs.outputFile(filepath, data, (error) => {
+    // this._md.renderer.rules.table_open = function () {
+    //     return '<table class="mrk table">\n';
+    // };
+  }
 
-                if (error) {
-                    return reject(error);
-                }
-
-                resolve(filepath);
-
-            });
-        });
-
-    }
-
-    /**
-     * @private
-     */
-    _init () {
-
-        this._md = new MarkdownIt({
-            highlight: (code, type) => {
-
-                var hl = code;
-
-                try {
-                    if (type) {
-                        hl = highlightJs.highlight(type, code).value;
-                    } else {
-                        hl = highlightJs.highlightAuto(code).value;
-                    }
-                } catch (error) {
-                    this._logger.error(error);
-                }
-
-                return hl;
-            },
-
-            typographer: true,
-            linkify: true,
-            html: true
-        });
-
-        this._md.use(plugins.taskLists, {label: true});
-        this._md.use(plugins.emoji);
-        this._md.use(plugins.footnote);
-        this._md.use(plugins.sub);
-        this._md.use(plugins.sup);
-        this._md.use(plugins.ins);
-        this._md.use(plugins.mark);
-        // this._md.use(plugins.abbr);
-        // this._md.use(mdContainer);
-
-        this._md.use(plugins.anchor, {
-            level: 1,
-            slugify: this._slugify,
-            permalink: true,
-            permalinkClass: 'header-anchor',
-            permalinkSymbol: '#',
-            permalinkBefore: false
-        });
-
-        // this._md.renderer.rules.table_open = function () {
-        //     return '<table class="mrk table">\n';
-        // };
-
-    }
-
-    /**
-     * @private
-     * @param {String} text
-     * @return {String}
-     */
-    _slugify (text) {
-        var slug = text.toLowerCase()
-          .replace(/\s/g, '-')
-          .replace(/[^\w-]/g, '');
-
-        return slug;
-    }
-
+  /**
+   * @private
+   * @param {String} text
+   * @return {String}
+   */
+  _slugify(text: string): string {
+    return text
+      .toLowerCase()
+      .replace(/\s/g, "-")
+      .replace(/[^\w-]/g, "");
+  }
 }
